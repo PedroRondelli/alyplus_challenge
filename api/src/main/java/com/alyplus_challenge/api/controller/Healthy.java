a0d06a2063aeae71adca5258d4a36d8be3448d6b
Pedro Rondelli <pedrorondelli@hotmail.com>
2026-01-10 16:51:45 -0300

    feat: Health endpoint

---
+++ api/src/main/java/com/alyplus_challenge/api/controller/Healthy.java
@@ -0,0 +1,17 @@
+package com.alyplus_challenge.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("api/healthy")
+
+
+public class Healthy {
+    @GetMapping
+    public String healthCheck() {
+        return "API is healthy";
+    }
+}
